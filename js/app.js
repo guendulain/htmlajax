396db68997cf83ae3ed57788198a3285388917f4
guendulain <alba.niddia@gmail.com>
2018-11-28 15:37:52 -0600

    inserción de estudiante completada

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 //Obtener referencia al elemento
 var btnActualizar= document.getElementById('btnActualizar');
+var btnGuardar= document.getElementById('btnGuardar');
+
 
 //Agregar un event listener para cada click
 btnActualizar.addEventListener('click',actualizar);
+btnGuardar.addEventListener('click',guardar);
 
 actualizar();
 
@@ -43,8 +46,43 @@ function actualizar(){
 
       }
     }
+
   };
   xhttp.open("GET","http://nyc.pixan.io/ajax/public/api/students",true);
   xhttp.send();
+}
 
+function guardar(){
+  var first_name = document.getElementById('nombre').value;
+  var last_name = document.getElementById('apellido').value;
+  var email = document.getElementById('correo').value;
+  var phone_number = document.getElementById('telefono').value;
+
+  var data=new FormData();
+  data.append('first_name',first_name);
+  data.append('last_name',last_name);
+  data.append('email',email);
+  data.append('phone_number',phone_number);
+
+  var xhttp= new XMLHttpRequest();
+  xhttp.onreadystatechange = function()
+  {
+    if (this.readyState == 4 && this.status == 200)
+    {
+      var response = JSON.parse(this.responseText);
+      if(response.status=='error'){
+        alert(response.errors[0]);
+      }
+      else{
+        actualizar();
+
+        document.getElementById('nombre').value="";
+        document.getElementById('apellido').value="";
+        document.getElementById('correo').value="";
+        document.getElementById('telefono').value="";
+      }
+    }
+  };
+  xhttp.open("POST","http://nyc.pixan.io/ajax/public/api/students",true);
+  xhttp.send(data);
 }
