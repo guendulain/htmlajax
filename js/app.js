208e0741a7aa8ae654edbad827a2f0c63071414c
guendulain <alba.niddia@gmail.com>
2018-11-29 11:27:56 -0600

    ya borré todo, jajaja

--- js/app.js
+++ js/app.js
@@ -26,23 +26,36 @@ function actualizar(){
           var idCell = document.createElement('td');
           var firstNameCell = document.createElement('td');
           var lastNameCell = document.createElement('td');
+          var deleteButtonCell = document.createElement('td');
+          var deleteButton = document.createElement('button');
+          deleteButton.className="Eliminar";
 
           var idText=document.createTextNode(student.id);
           var firstNameText=document.createTextNode(student.first_name);
           var lastNameText=document.createTextNode(student.last_name);
+          var deleteButtonText=document.createTextNode("Eliminiar");
+
 
 
           idCell.appendChild(idText);
           firstNameCell.appendChild(firstNameText);
           lastNameCell.appendChild(lastNameText);
+          deleteButton.appendChild(deleteButtonText);
+          deleteButtonCell.appendChild(deleteButton);
 
           row.appendChild(idCell);
           row.appendChild(firstNameCell);
           row.appendChild(lastNameCell);
+          row.appendChild(deleteButtonCell);
 
           document.getElementsByTagName('tbody')[0].appendChild(row);
 
         });
+        var botonesEliminar = document.getElementsByClassName('Eliminar');
+        for(var i=0;i<botonesEliminar.length;i++){
+          botonesEliminar[i].addEventListener('click',eliminarUsuario(response.students[i]));
+
+        }
 
       }
     }
@@ -51,7 +64,21 @@ function actualizar(){
   xhttp.open("GET","http://nyc.pixan.io/ajax/public/api/students",true);
   xhttp.send();
 }
-
+ function eliminarUsuario(student){
+   return function(event){
+  // var data = new FormData();
+   //data.append('_method','DELETE');
+   var xhr =  new XMLHttpRequest();
+   xhr.onreadystatechange=function(){
+       if(this.readyState==4 && this.status==200)
+       {
+          event.target.parentNode.parentNode.parentNode.removeChild(event.target.parentNode.parentNode);
+       }
+     };
+     xhr.open("DELETE","http://nyc.pixan.io/ajax/public/api/students/"+student.id,true);
+     xhr.send();
+   }
+ }
 function guardar(){
   var first_name = document.getElementById('nombre').value;
   var last_name = document.getElementById('apellido').value;
